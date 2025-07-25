402604796fb8426b36633f886dc71f7917c6b0c5
Quising, Ashly Jana A. <ashlyjanaquising@gmail.com>
2025-07-26 00:40:43 +0800

    Add files via upload

---
+++ act34.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ass1act34
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            Console.Write("Enter a positive integer: ");
+            number = int.Parse(Console.ReadLine());
+
+            while (number >= 10) // Repeat until the number becomes single-digit
+            {
+                int sum = 0;
+                while (number > 0)
+                {
+                    sum += number % 10; // Add the last digit
+                    number /= 10;       // Remove the last digit
+                }
+                number = sum; // Update number with the new sum
+            }
+
+            Console.WriteLine("Digital Root: " + number);
+        }
+    }
+}
